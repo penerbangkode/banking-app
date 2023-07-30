40f9bcb5898f1dfef56b772798f8f5bba79535e7
Rido Mulanda <rido@mifx.com>
2023-07-30 20:01:33 +0700

    master amount title table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -423,7 +423,7 @@
                 },
                 {
                     field: "amount",
-                    title: "",
+                    title: "amount",
                     width: 200,
                     // callback function support for column rendering
                     template: function(row) {
